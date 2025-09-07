e9e00c7954868f6c8f0a7be58f921ed4f6749bac
Anush BG <133494634+BgAnush@users.noreply.github.com>
2025-09-07 16:25:57 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from models.translate_models import TranslateRequest
+from .models.translate_models import TranslateRequest
 from utils.translate_helper import translate_text, translate_to_all
 
 router = APIRouter()
@@ -13,3 +13,4 @@ def translate(req: TranslateRequest):
 def translate_multi(req: TranslateRequest):
     """Translate text into all 5 supported languages"""
     return translate_to_all(req.text)
+
