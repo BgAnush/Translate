e3e32fb81808f984b161d225a46b9dd490e221d4
Anush BG <133494634+BgAnush@users.noreply.github.com>
2025-09-20 19:21:21 +0530

    Delete models directory

--- models/translate_models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Optional
-
-class TranslateRequest(BaseModel):
-    texts: List[str]  # Accept a list of texts
-    target: str       # Target language code, e.g., "kn"
