8cafc849e6a1b054b05b31e3651d45f8f4d91c6a
Anush BG <133494634+BgAnush@users.noreply.github.com>
2025-09-24 20:10:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
-from app.models import TranslateRequest
+from fastapi.middleware.cors import CORSMiddleware
+from app.models import TranslateRequest, TranslateResponse
 from app.translator import translate_text
 
 app = FastAPI(title="FarmerChat Translator API", version="1.0")
+
+# Allow all origins for simplicity
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def root():
     return {"message": "FarmerChat Translator API is running!"}
 
-@app.post("/translate")
+@app.post("/translate", response_model=TranslateResponse)
 def translate(request: TranslateRequest):
     """
     Translate input text to the target language.
     Example JSON:
     {
-        "text": "मेरा नाम अनुष है",
+        "text": "mera naam anush he",
         "target_language": "kn"
     }
     """
